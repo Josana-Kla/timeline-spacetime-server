fc06add3b02e63f68d126192c786557703c6d306
Josana-Kla <josana.0205@gmail.com>
2023-05-30 18:16:37 -0300

    feat: added oauth with github and jwt

---
+++ src/routes/auth.ts
@@ -0,0 +1,80 @@
+import { FastifyInstance } from 'fastify'
+import axios from 'axios'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+
+export async function authRoutes(app: FastifyInstance) {
+  app.post('/register', async (request) => {
+    const bodySchema = z.object({
+      code: z.string(),
+    })
+
+    const { code } = bodySchema.parse(request.body)
+
+    // primeiro parametro é o endereço do github, o segundo é corpo da requisição, e o terceiro configurações da requisição
+    const accessTokenResponse = await axios.post(
+      'https://github.com/login/oauth/access_token',
+      null,
+      {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: 'application/json', // para dizer para o github como vc quer que ele retorne o formato da resposta da requisição
+        },
+      },
+    )
+
+    const { access_token } = accessTokenResponse.data
+
+    const userResponse = await axios.get('https://api.github.com/user', {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    })
+
+    const userSchema = z.object({
+      id: z.number(),
+      login: z.string(),
+      name: z.string(),
+      avatar_url: z.string().url(),
+    })
+
+    const userInfo = userSchema.parse(userResponse.data)
+
+    let user = await prisma.user.findUnique({
+      where: {
+        githubId: userInfo.id,
+      },
+    })
+
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          githubId: userInfo.id,
+          login: userInfo.login,
+          name: userInfo.name,
+          avatar: userInfo.avatar_url,
+        },
+      })
+    }
+
+    // primeiro objeto é quais informações do usuário eu quero que esteja contidas dentro do token -colocar dados que não são sensíveis-. O segundo objeto, o sub é para falar a qual usuário pertence o token - coloque uma informação que seja única-
+    const token = app.jwt.sign(
+      {
+        name: user.name,
+        avatarUrl: user.avatar,
+      },
+      {
+        sub: user.id,
+        expiresIn: '30 days',
+      },
+    )
+
+    return {
+      token,
+    }
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
+import 'dotenv/config'
 import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
+app.register(cors, {
+  origin: true, // todas urls do front podem acessar o back
+})
+app.register(jwt, {
+  secret: 'spacetime', // maneira de diferenciar os tokens gerados por esse backend de outros jwts gerados por outros backend. Algo que não seja fácil de conter em outras aplicações
+})
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
